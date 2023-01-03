a7f5920f14badcc78640cec164b3a4aef2011bd8
Irving Juárez <irvingjuarez274@gmail.com>
2023-01-03 15:32:11 -0600

    Creation of the very first geometric figure with three js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,24 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+camera.position.z = 6;
+camera.position.x = 2;
+camera.position.y = 2
+
+const meshConfig = {
+	color: "#fff"
+}
+
+const boxGeometry = new THREE.BoxGeometry();
+const boxMaterial = new THREE.MeshBasicMaterial(meshConfig);
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+scene.add(box)
+
+renderer.render(scene, camera)
