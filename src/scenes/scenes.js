b9bbe84cec48343ee9d2fbbb9887e5520f55aab1
Irving Juárez <irvingjuarez274@gmail.com>
2023-01-09 12:24:10 -0600

    Responsiveness added to the scene

--- src/scenes/scenes.js
+++ src/scenes/scenes.js
@@ -1,23 +1,30 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
 
+// Setting up
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement)
+const canvas = renderer.domElement
 
+// Camera - Controls - Scene
 const fov = 40;
 const aspect = window.innerWidth / window.innerHeight;
 const near = 2;
 const far = 10;
+
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
-
 const controls = new OrbitControls(camera, renderer.domElement)
-
 const scene = new THREE.Scene()
-scene.background = new THREE.Color(0xAAAAAA)
 
-camera.position.z = 13
+camera.position.set(0, 8, 11)
 controls.update()
+
+// Adding light into the scene
+const lightColor = "white"
+const intensity = 3
+const light = new THREE.PointLight(lightColor, intensity)
+scene.add(light)
 
 // An array of objects whose rotation to update
 const objects = []
@@ -35,8 +42,31 @@ sun.scale.set(5, 5, 5)
 scene.add(sun)
 objects.push(sun)
 
-function animate() {
+function resizeCanvas() {
+	const pixelRatio = window.devicePixelRatio
+	const resizeWidth = canvas.width * pixelRatio !== window.innerWidth
+	const resizeHeight = canvas.height * pixelRatio !== window.innerHeight
+	const resizingResult = resizeWidth || resizeHeight
+
+	if (resizingResult) {
+		renderer.setSize(window.innerWidth, window.innerHeight)
+	}
+
+	return resizingResult
+}
+
+function animate(time) {
 	requestAnimationFrame(animate)
+
+	if (resizeCanvas()) {
+		camera.aspect = canvas.clientWidth / canvas.clientHeight
+		camera.updateProjectionMatrix()
+	}
+
+	objects.forEach(obj => {
+		obj.rotation.x = time / 1000
+		obj.rotation.y = time / 1000
+	})
 
 	controls.update()
 	renderer.render(scene, camera)
