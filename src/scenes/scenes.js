45d99371ffaec81d7a3488daf4645dcbf87922ca
Irving Juárez <irvingjuarez274@gmail.com>
2023-01-09 11:57:46 -0600

    orbit controls added

--- src/scenes/scenes.js
+++ src/scenes/scenes.js
@@ -1,18 +1,23 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement)
 
 const fov = 40;
-const aspect = 2;
+const aspect = window.innerWidth / window.innerHeight;
 const near = 2;
 const far = 10;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
-camera.position.z = 13
+
+const controls = new OrbitControls(camera, renderer.domElement)
 
 const scene = new THREE.Scene()
 scene.background = new THREE.Color(0xAAAAAA)
+
+camera.position.z = 13
+controls.update()
 
 // An array of objects whose rotation to update
 const objects = []
@@ -28,5 +33,12 @@ const sun = new THREE.Mesh(sunGeometry, sunMaterial);
 sun.scale.set(5, 5, 5)
 
 scene.add(sun)
+objects.push(sun)
 
-renderer.render(scene, camera)
+function animate() {
+	requestAnimationFrame(animate)
+
+	controls.update()
+	renderer.render(scene, camera)
+}
+requestAnimationFrame(animate)
